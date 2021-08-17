a5f2476a0504bce41e9bf13c020f4e4b1f047fc5
elchanan <elchananmahatsri@gmail.com>
2021-08-17 22:15:28 +0300

    socketio changed

---
+++ app/src/main/java/com/project/courier_app/SocketIO.java
@@ -0,0 +1,21 @@
+package com.project.courier_app;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+public class SocketIO {
+    private static Socket mSocket;
+    public static Socket getSocket()
+    {
+        if(mSocket!= null && mSocket.isActive())
+            return mSocket;
+        try {
+             mSocket = IO.socket(RetrofitBase.BASE_URL);
+            mSocket.connect();
+        } catch ( URISyntaxException e) {}
+        return mSocket;
+    }
+
+
+}
